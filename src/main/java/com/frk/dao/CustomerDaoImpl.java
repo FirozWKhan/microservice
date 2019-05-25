9e8e8531b4e270e7aab70b6adc37d264ca27bba3
firozwkhan92 <firozwkhan92@gmail.com>
2019-05-25 12:46:28 +0530

    Adding get all services

--- src/main/java/com/frk/dao/CustomerDaoImpl.java
+++ src/main/java/com/frk/dao/CustomerDaoImpl.java
@@ -1,5 +1,6 @@
 package com.frk.dao;
 
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
@@ -7,9 +8,11 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.frk.entity.CustomerEntity;
+import com.frk.jparepository.CustomerRepository;
 import com.frk.model.Customer;
 
 @Repository
@@ -18,6 +21,9 @@ public class CustomerDaoImpl {
 
 	
 	  @PersistenceContext EntityManager em;
+	  
+	  @Autowired
+	  CustomerRepository cr;
 	 
 	
 	public String saveCustomer(Customer cr) {
@@ -28,6 +34,11 @@ public class CustomerDaoImpl {
 		em.persist(ce);
 		return "done";
 		
+	}
+	
+	public List<CustomerEntity> getCustomerAll(){
+		List<CustomerEntity> crlist = (List<CustomerEntity>) cr.findAll();
+		return crlist;
 	}
 	
 }
---
+++ src/main/java/com/frk/service/CustomerService.java
@@ -0,0 +1,34 @@
+package com.frk.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.frk.dao.CustomerDaoImpl;
+import com.frk.entity.CustomerEntity;
+import com.frk.model.Customer;
+
+@Service
+public class CustomerService {
+	
+	@Autowired
+	CustomerDaoImpl cr;
+	
+	public List<Customer> getAllCustomer(){
+		List<CustomerEntity> customerAll = cr.getCustomerAll();
+		List<Customer> cm = new  ArrayList<Customer>();
+				
+		for (CustomerEntity ce : customerAll) {
+			Customer m = new Customer();
+			m.setFname(ce.getFname());
+			m.setLname(ce.getLname());
+			cm.add(m);
+		}
+		
+		return cm;
+	}
+	
+	
+}
