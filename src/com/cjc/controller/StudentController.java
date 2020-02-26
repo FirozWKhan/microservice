a96ae65d20ec9288e78fe49579f1b435b0c16e7b
lenovo <lenovo@LAPTOP-KDBJCCPB>
2020-02-26 23:25:36 +0530

    add sosy

--- src/com/cjc/controller/StudentController.java
+++ src/com/cjc/controller/StudentController.java
@@ -6,6 +6,8 @@ public class StudentController {
 		System.out.println("hlw firoz khan");
 		System.out.println("hlw afiya");
 		System.out.println("hlw aliya");
+		System.out.println("hlw aliyaaaaa");
+
 	}
 
 }
